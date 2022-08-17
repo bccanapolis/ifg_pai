af15fdf5e0671b2ad36605994e6dd973bf64f591
Bruno de Araujo Alves <me@baraus.dev>
2022-08-17 14:42:08 -0300

    Updates working!

--- config/web.php
+++ config/web.php
@@ -116,6 +116,7 @@ if (YII_ENV_DEV) {
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['*']
     ];
 
     $config['bootstrap'][] = 'gii';
@@ -123,6 +124,7 @@ if (YII_ENV_DEV) {
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['*']
     ];
 }
 
--- web/index.php
+++ web/index.php
@@ -1,12 +1,16 @@
 <?php
 
-// comment out the following two lines when deployed to production
-//defined('YII_DEBUG') or define('YII_DEBUG', true);
-//defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
 
+// registra o autoloader do Composer
 require __DIR__ . '/../vendor/autoload.php';
+
+// inclui o arquivo da classe Yii
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
+// carrega a configuração da aplicação
 $config = require __DIR__ . '/../config/web.php';
 
+// cria, configura e executa a aplicação
 (new yii\web\Application($config))->run();
