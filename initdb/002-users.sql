6370cfcf6a53236c91594abe46c0b05a3d08214d
Bruno de Araujo Alves <me@baraus.dev>
2022-10-19 09:52:40 -0300

    Update user

--- initdb/002-users.sql
+++ initdb/002-users.sql
@@ -1,2 +1,2 @@
 ﻿INSERT INTO "user" (id, username, auth_key, password_hash, confirmation_token, status, superadmin, created_at, updated_at, registration_ip, bind_to_ip, email, email_confirmed) VALUES
-(1, 'superadmin', 'kz2px152FAWlkHbkZoCiXgBAd-S8SSjF', '$2y$13$6Usx8yH666k6t5dc0mGMhOZRVN182F1A2nJRnC3ZAG5YXpFrH6l1y', NULL, 1, 1, 1426062188, 1426062188, NULL, NULL, NULL, 0);
+(1, 'superadmin', 'kz2px152FAWlkHbkZoCiXgBAd-S8SSjF', '$2y$13$lsQEEFh4C7yL6GtkvJnr5e87MDBKzR8SzmqZ/JdFQsspw.Bd9xkZW', NULL, 1, 1, 1426062188, 1426062188, NULL, NULL, NULL, 0);
